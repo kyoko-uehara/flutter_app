be7443d1cce0a9ae124f14db8cee3aaede042b98
kyoko-uehara <kyoko.uehara@stv-tech.co.jp>
2021-03-30 17:03:35 +0900

    SourceTreeにてcommit

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'uehara test'),
+      home: MyHomePage(title: 'SourceTree test'),
     );
   }
 }
